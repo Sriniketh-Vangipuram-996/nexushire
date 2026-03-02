5d66397943288c7e1ac792634a256fad50c65f42
Sriniketh <srinikethvangipuram996@gmail.com>
2026-03-02 21:57:15 +0530

    feat(db): add migrate-mongo with rollback support

---
+++ backend/migrations/20260302162021-add-user-email-index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {  //up=apply
+    await db.collection("users").createIndex(
+      {email:1},
+      {unique:true}
+    );
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {  //down = rollback
+    await db.collection("users").dropIndex("email_1");
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  }
+};
