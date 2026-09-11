bc6f761d80b624eba1cf2d376cb8a2375a93da0b
Sriniketh-Vangipuram <sriniketh202@gmail.com>
2026-09-11 14:01:34 +0530

    Fix Gmail SMTP for Render using STARTTLS

--- backend/src/config/email.ts
+++ backend/src/config/email.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
-  secure: process.env.EMAIL_SECURE==="true",
-  tls: {
-    rejectUnauthorized: false, // allows self-signed cert
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // IMPORTANT
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
